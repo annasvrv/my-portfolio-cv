fa2b93295cd1253399c09ef4e797e1f32751a23e
“Anna <suvorova.a@brainrocket.com>
2025-07-07 11:19:24 +0300

    created main banner for home page. need to finish.

---
+++ src/app/components/main-banner/main-banner.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'stb-main-banner',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './main-banner.component.html',
+  styleUrl: './main-banner.component.scss',
+})
+export class MainBannerComponent {
+  
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
 
 @Component({
   selector: 'stb-home',
-  imports: [],
+  imports: [MainBannerComponent, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  
 }
